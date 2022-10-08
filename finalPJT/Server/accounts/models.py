5a330e8a841b20a2d8bb652ce7ae983ddae8716c
ParkJoungWoo <jug78@naver.com>
2022-10-09 02:22:22 +0900

    1차 모델 설계

--- finalPJT/Server/accounts/models.py
+++ finalPJT/Server/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import datetime
 # Create your models here.
+
+# AbstractUser에 기본적으로 id와 password가 있음
+class User(AbstractUser):
+    
+    # id(PK, index), username(로그인 id), pw(로그인 pw)
+    # 유저 id
+    user_id = models.CharField(max_length=10, unique=True, )
+    # 유저 비밀번호
+    password = models.CharField(max_length=10)
+    # 유저 주소
+    address = models.CharField(max_length=10)
+    # 유저의 닉네임
+    nickname = models.CharField(max_length=10)
+    # USERNAME_FIELD = 'userId'    
+    # Menu테이블과 1:n으로 연결 필요
+
+# userName이 AbstractUser에 기본 제공되어 duplicate column name: userName Error 발생!
+# createsuperuser 생성 시 default 값이 없으면 migrate가 잘 되어도 data가 들어가지 않는 이슈 발생.
---
+++ finalPJT/Server/accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+# from rest_framework_jwt.views import obtain_jwt_token
+app_name = 'accounts'
+
+urlpatterns = [
+    # 회원가입
+    path('join/', views.join),
+    
+    # 로그인
+    path('api-token-auth/', obtain_jwt_token),
+    #  path('login/', views.login),
+    
+    # 아이디 중복 확인
+    path('id/<str:username>/', views.check_id),
+    
+    # 회원정보 업데이트
+    path('update/', views.update_user_info),
+]
