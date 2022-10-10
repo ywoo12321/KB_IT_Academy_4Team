f69fb818a853c276e20b7f89b34da5ac6894215c
ParkJoungWoo <jug78@naver.com>
2022-10-11 00:55:49 +0900

    is_unique 작업 완, 회원 등록 진행중

--- finalPJT/Server/accounts/views.py
+++ finalPJT/Server/accounts/views.py
@@ -1,3 +1,4 @@
+from gc import get_objects
 from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, get_list_or_404
@@ -8,16 +9,16 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 
+from finalPJT.Server.accounts.serializers import AccountSerializer, PreferSerializer
 from .models import Account, Prefer
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def user_test(request, user_id):
-    print(user_id)
-    user = get_object_or_404(Account, pk=user_id)
-    print(user)
-    return Response(user.nickname)
-
+def is_unique(request, user_id):
+    user = Account.objects.get(user_id=user_id)
+    if user is None:
+        return JsonResponse({"isUnique":True})
+    return JsonResponse({"isUnique":False})
     
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -29,3 +30,21 @@ def prefer_test(request, user_id):
         'provence' : prefer.prefer_provence,
     }
     return JsonResponse(content)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def join(request):
+    if request.method=='POST':
+        user_id = request.data['userid']
+        # prefer = 
+        check = get_objects(Account, pk=user_id)
+        if check is None:
+            account_serializer = AccountSerializer(data=request.data)
+            account_serializer.save()
+            # prefer 정보를 어떻게 전달? 문자열로 전달?
+            if request.data['']
+            prefer_serializer = PreferSerializer(data=)
+        else : 
+            return JsonResponse({"code":200, "message":"Fail, 이미 존재한 아이디"})    
+    else:
+        return JsonResponse({"code":200, "message":"Fail, 잘못된 요청"})
