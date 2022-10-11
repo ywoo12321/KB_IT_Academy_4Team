e16ffcaf07459439362d700b5696c0c5a7714ea0
ywoo12321 <ywoo12321@gmail.com>
2022-10-11 17:25:49 +0900

    prettier, eslint, setting.json 설정

--- finalPJT/front/src/App.js
+++ finalPJT/front/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>kb 아카데미</div>
+      <div>4조 입니다.</div>
+    </>
   );
-}
+};
 
 export default App;
