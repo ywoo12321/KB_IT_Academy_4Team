9e4f732102fb53c5402f919dcb79a04cad9151f4
ywoo12321 <ywoo12321@gmail.com>
2022-10-12 16:23:01 +0900

    theme 설정(글씨체, 글씨 크기, 색상) 완료

--- finalPJT/itsurlife/src/App.js
+++ finalPJT/itsurlife/src/App.js
@@ -1,9 +1,13 @@
 import styled from "@emotion/styled";
+import theme from "./styles/theme";
 
 const App = () => {
   return (
     <NavBox>
-      <p>ddd</p>
+      <p>
+        나만의
+        <br /> 감성숙소를 찾아주다
+      </p>
     </NavBox>
   );
 };
@@ -12,9 +16,11 @@ export default App;
 
 const NavBox = styled.div`
   width: 100%;
-  height: 60px;
+  height: 300px;
   background-color: blue;
   & > p {
-    color: #ffffff;
+    color: ${theme.color.whiteFont};
+    font-family: ${theme.font_family.T};
+    font-size: ${theme.font_size.tittle};
   }
 `;
---
+++ finalPJT/itsurlife/src/styles/theme.js
@@ -0,0 +1,25 @@
+const theme = {
+  color: {
+    main: "#000000",
+    whiteFont: "#FFFFFF",
+    point: "#DCFC73",
+  },
+  font_size: {
+    tittle: "5.625rem",
+    h1: "2.5rem",
+    h2: "2.00rem",
+    h3: "1.75rem",
+    h4: "1.63rem",
+    h5: "1.50rem",
+    subtitle1: "1.38rem",
+    subtitle2: "1.25rem",
+    body1: "1.13rem",
+    body2: "1.00rem",
+  },
+  font_family: {
+    T: "Happiness-Sans-Bold",
+    N: "NanumBarunGothic",
+    B: "NanumBarunGothicBold",
+  },
+};
+export default theme;
