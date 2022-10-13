e8614ca1e2e5d2871a565fa3f528a9847a2d4bbc
ywoo12321 <ywoo12321@gmail.com>
2022-10-13 11:40:22 +0900

    Randing Page 첫번째 칸 디자인 구현완료

--- finalPJT/itsurlife/src/pages/App.js
+++ finalPJT/itsurlife/src/pages/App.js
@@ -1,26 +1,20 @@
 import styled from "@emotion/styled";
 import theme from "../styles/theme";
+import LandingPage from "./LandingPage";
 
 const App = () => {
   return (
-    <NavBox>
-      <p>
-        나만의ddd
-        <br /> 감성숙소를 찾아주다ㅇㅇ
-      </p>
-    </NavBox>
+    <>
+      <LandingPage />
+      <NavBox />
+    </>
   );
 };
 
 export default App;
 
 const NavBox = styled.div`
-  width: 100%;
-  height: 300px;
-  background-color: blue;
-  & > p {
-    color: ${theme.color.whiteFont};
-    font-family: ${theme.font_family.T};
-    font-size: ${theme.font_size.tittle};
-  }
+  width: 1920px;
+  height: 79px;
+  background-color: #7097a8;
 `;
---
+++ finalPJT/itsurlife/src/pages/LandingPage.js
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+import theme from "../styles/theme";
+import Randing1 from "../images/Landing1.png";
+
+const LandingPage = () => {
+  return (
+    <FirstBox>
+      <BigImageBox>
+        <img src={Randing1} width="864" height="1080" alt="Landing1" />
+      </BigImageBox>
+      <FirstRightBox>
+        <FirstTextBox>
+          <p>
+            나만의
+            <br /> 감성숙소를 찾아주다
+          </p>
+        </FirstTextBox>
+        <GoToMainBtn>
+          <div className="first">
+            <p>둘러보기</p>
+          </div>
+        </GoToMainBtn>
+      </FirstRightBox>
+    </FirstBox>
+  );
+};
+
+export default LandingPage;
+
+const FirstBox = styled.div`
+  width: 1920px;
+  height: 1080px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const BigImageBox = styled.div`
+  width: 864px;
+  height: 1080px;
+`;
+const FirstRightBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const FirstTextBox = styled.div`
+  width: 800px;
+  height: 280px;
+  margin-top: 654px;
+  margin-right: 76px;
+  text-align: right;
+  & > p {
+    font-family: ${theme.font_family.T};
+    font-size: ${theme.font_size.tittle};
+  }
+`;
+const GoToMainBtn = styled.div`
+  & > .first {
+    margin-top: 20px;
+    margin-bottom: 158px;
+    margin-right: 76px;
+    text-align: right;
+    & > p {
+      font-family: ${theme.font_family.N};
+      font-size: ${theme.font_size.h2};
+      color: ${theme.color.main};
+      opacity: 0.8;
+    }
+  }
+`;
