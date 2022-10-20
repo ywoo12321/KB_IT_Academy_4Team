1ec25a9901a1d1341d7d84557510b95205964b76
AhnSeonWoo <AhnSeonWoo>
2022-10-20 17:34:29 +0900

    회원가입 페이지 완성

--- finalPJT/itsurlife/src/components/SignupForm.js
+++ finalPJT/itsurlife/src/components/SignupForm.js
@@ -1,83 +1,141 @@
 import Btn from "./Btn";
 import styled from "@emotion/styled";
 import theme from "../styles/theme";
+import Signupimg from "../images/Signupimg.png";
 
 const SignupForm = () => {
   return (
-    <Form className="login">
-      <Title>당신에 대하여 알려주세요</Title>
-      <div>
-        <span>ID</span>
-        <input type="text" name="id" />
-      </div>
-      <div>
-        <span>ID</span>
-        <input type="text" name="id" />
-      </div>
-      <div>
-        <span>ID</span>
-        <input type="text" name="id" />
-      </div>
-      <div>
-        <span>ID</span>
-        <input type="text" name="id" />
-      </div>
-      <div>
-        <span>PW</span>
-        <input type="password" name="pw" />
-      </div>
-      <div className="btncontainer">
-        <Btn>홈으로</Btn>
-        <Btn>로그인</Btn>
-      </div>
-    </Form>
+    <>
+      <ImgBox>
+        <img src={Signupimg} alt="." />
+      </ImgBox>
+      <FormBox>
+        <Form className="login">
+          <Title>당신에 대하여 알려주세요</Title>
+          <div>
+            <p>ID</p>
+            <input
+              type="text"
+              name="id"
+              placeholder="영어와 숫자를 이용하여 3~6자로 생성해주세요"
+            />
+            <Btn className="checkBtn">중복 확인</Btn>
+          </div>
+          <div>
+            <p>PW</p>
+            <input
+              type="password"
+              name="pw"
+              placeholder="영어와 숫자와 특수문자를 이용하여 6~18자로 생성해주세요."
+            />
+          </div>
+          <div>
+            <p>PW 확인</p>
+            <input
+              type="password"
+              name="checkpw"
+              placeholder="위에서 작성한 pw를 다시 한 번 작성해주세요."
+            />
+          </div>
+          <div>
+            <p>닉네임</p>
+            <input type="text" name="nickname" placeholder="사용하실 닉네임을 작성해주세요." />
+          </div>
+          <div>
+            <p>거주지역</p>
+            <input type="text" name="area" />
+          </div>
+          <div className="btncontainer">
+            <Btn>이전</Btn>
+            <Btn>다음</Btn>
+          </div>
+        </Form>
+      </FormBox>
+    </>
   );
 };
 
 export default SignupForm;
 
+const ImgBox = styled.div`
+  display: block;
+  width: 35%;
+  height: 1000px;
+  background: red;
+  float: left;
+
+  & > img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const FormBox = styled.div`
+  display: block;
+  width: 65%;
+  height: 1000px;
+  float: right;
+`;
+
 const Form = styled.form`
-  background-color: ${theme.color.point};
-  border-radius: 40px;
+  display: block;
+  background-color: ${theme.color.whiteFont};
   text-align: center;
-  width: 1280px;
-  margin: 175px auto 175px auto;
+  margin: 123px 130px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 40px 40px 40px 46px;
 
   & {
-    height: 570px;
+    height: 745px;
   }
 
   & > div {
-    text-align: center;
+    text-align: start;
   }
-  & > div > span {
+  & > div > button {
+    margin-left: 20px;
+  }
+  & > div > p {
     display: inline-block;
-    text-align: center;
-    width: 65px;
-    font-size: ${theme.font_size.h2};
+    text-align: end;
+    padding-right: 13px;
+    width: 150px;
+    font-size: ${theme.font_size.h3};
+    font-family: ${theme.font_family.N};
     margin-right: 14px;
+    margin-left: 40px;
   }
 
   & > div > input {
+    width: 500px;
     border-radius: 30px;
     border: none;
     height: 55px;
-    margin-bottom: 58px;
+    margin-bottom: 40px;
     padding-left: 20px;
+    outline: none;
     font-size: ${theme.font_size.subtitle1};
     font-family: ${theme.font_family.T};
-  }
+    background: #f6f5f1;
+    border: 1px solid ${theme.color.logoColor};
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
-  & > div > input[name="id"] {
-    width: 500px;
+    ::placeholder {
+      font-family: ${theme.font_family.N};
+      font-size: 16px;
+    }
   }
-
   & > div > input[name="pw"] {
-    width: 500px;
+    width: 683px;
+  }
+
+  & > .btncontainer {
+    text-align: center;
   }
 
   & > .btncontainer > button {
-    margin: 30px 75px 20px 75px;
+    margin: 10px 75px 20px 75px;
   }
 `;
 
